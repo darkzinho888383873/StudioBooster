378574b47ac8025ba912722cce70cf2bee4fdbd1
S1d7 <rafaelcohsirhfbr@gmail.com>
2026-07-09 17:15:04 -0300

    Add settings.gradle.kts for project configuration

---
+++ .github/workflows/settings.gradle.kts
@@ -0,0 +1,18 @@
+// settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StudioBooster"
+include(":app")
